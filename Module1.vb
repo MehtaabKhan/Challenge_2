40a5ffd6e68f26816be99230a44c92afd66f927c
Mehtaab Khan <22mkhan@heckgrammar.co.uk>
2024-07-15 11:06:15 +0100

    asd

--- Module1.vb
+++ Module1.vb
@@ -1,7 +1,15 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("hello")
+        Dim testScores As Integer
+        Dim totalTests As Integer
+
+        While totalTests <> 10
+            Console.WriteLine("Student Score: ")
+            testScores = Console.ReadLine()
+            totalTests = totalTests + 1
+            testScores = testScores + testScores
+        End While
     End Sub
 
 End Module
