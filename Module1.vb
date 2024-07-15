6de20301baa7cc952cc180ec5bf0402c1791738b
Mehtaab Khan <22mkhan@heckgrammar.co.uk>
2024-07-15 11:20:26 +0100

    a

--- Module1.vb
+++ Module1.vb
@@ -3,13 +3,17 @@
     Sub Main()
         Dim testScores As Integer
         Dim totalTests As Integer
+        Dim averageOfTotalScores As Decimal
 
         While totalTests <> 10
             Console.WriteLine("Student Score: ")
-            testScores = Console.ReadLine()2
+            testScores = Console.ReadLine()
             totalTests = totalTests + 1
             testScores = testScores + testScores
         End While
+
+        averageOfTotalScores = testScores / totalTests
+        Console.WriteLine("Average: " & averageOfTotalScores)
     End Sub
 
 End Module
