f65d366102420badafe76e28beeb754c40003271
Mehtaab Khan <22mkhan@heckgrammar.co.uk>
2024-07-18 09:34:01 +0100

    w

--- Module1.vb
+++ Module1.vb
@@ -2,18 +2,28 @@
 
     Sub Main()
         Dim testScores As Integer
+        Dim newTestScoreResults As Integer
         Dim totalTests As Integer
         Dim averageOfTotalScores As Decimal
 
         While totalTests <> 10
             Console.WriteLine("Student Score: ")
             testScores = Console.ReadLine()
+            TestScoreValidation(testScores)
             totalTests = totalTests + 1
-            testScores = testScores + testScores
+            averageOfTotalScores = newTestScoreResults + testScores
+            newTestScoreResults = testScores
         End While
 
         averageOfTotalScores = testScores / totalTests
         Console.WriteLine("Average: " & averageOfTotalScores)
     End Sub
 
+    Sub TestScoreValidation(score)
+        If score > 10 Then
+            Console.WriteLine("Score must be in range of 0-10")
+        ElseIf score < 0 Then
+            Console.WriteLine("Score must be in range of 0-10")
+        End If
+    End Sub
 End Module
